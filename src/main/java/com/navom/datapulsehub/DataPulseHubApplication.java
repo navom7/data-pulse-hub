4b06b88931432d8cde85048110a8344496bf93e3
Nanhe Kumar Pandey <navo.akp@gmail.com>
2024-02-22 18:37:55 +0530

    initial setup

---
+++ src/main/java/com/navom/datapulsehub/DataPulseHubApplication.java
@@ -0,0 +1,13 @@
+package com.navom.datapulsehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPulseHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataPulseHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/navom/datapulsehub/DataPulseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navom.datapulsehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPulseHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
